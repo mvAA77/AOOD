4f32a393c1788d84963b311dae219ceb99d14527
Tanya Kadiyala <123403349+mvAA77@users.noreply.github.com>
2025-09-02 12:11:51 -0400

    U1L1 Undo

--- DebuggingExercise-1/src/main/java/MyMultiplication.java
+++
@@ -1,9 +0,0 @@
-class MyMultiplication { // Class name and file name
-
-  public int multiple(int a, int b) {
-
-    return a * b;
-    
-  }
-  
-}
--- DebuggingExercise-1/src/main/java/MySum.java
+++
@@ -1,9 +0,0 @@
-class MySum { // Class name has to be the same as the file name
-
-  public int mySum(int a, int b) {
-
-    return a + b;
-    
-  }
-
-}
