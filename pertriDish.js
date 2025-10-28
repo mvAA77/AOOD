8f2a6fd156167d4149e111e0ee5f6d2269206634
Tanya Kadiyala <123403349+mvAA77@users.noreply.github.com>
2025-10-28 16:36:50 -0400

    Pertri Dish

--- pertriDish.js
+++ pertriDish.js
@@ -1,11 +1,27 @@
-const bacteria = {
-  initial: 10,
-  current: initial,
-  doublepop : function(s) {
+class PetriDish {
+  
+  
+  constructor(initial) {
+    this.initial = initial;
+    this.current = 1;
+  }
+
+
+  doublepop(sec) {
+    let s = sec/3
     this.current = this.initial *((2)^s)
     return "Bacteria Count: " + this.current
-  },
-  spray : function(s) {
-    this.current = this.current * 0.1
   }
-};
+
+  spray() {
+    this.current =Math.floor(this.current * 0.1);
+    return "Bacteria Count after spray: " + this.current;
+  }
+}
+
+const dish = new PetriDish(10);
+console.log(dish.doublepop(9));
+console.log(dish.spray());
+
+
+
